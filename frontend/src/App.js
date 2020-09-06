b054700f66d415d8df903e63c093b7108d296ce9
Mukilan1600 <ak-muki@hotmail.com>
2020-09-06 16:08:50 +0530

    Added event Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { index as LandingPage } from './LandingPage'
 import { index as ComingSoon } from './ComingSoon'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './utils/globalStyles'
 
 function App() {
     return <LandingPage /> && <ComingSoon />
