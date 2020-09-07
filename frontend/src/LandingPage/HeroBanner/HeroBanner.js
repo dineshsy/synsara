0ba6fba0d8ab6abc037ac0503b14d544d94dcd51
Vijai <vijayselvanvj@gmail.com>
2020-09-07 12:31:50 +0530

    Revert "Revert "landingpage""
    This reverts commit 6dabaa30bf8ea11b18d14832774e68accccea22a.

--- frontend/src/LandingPage/HeroBanner/HeroBanner.js
+++ frontend/src/LandingPage/HeroBanner/HeroBanner.js
@@ -1,6 +1,36 @@
 import React from 'react'
+import { LandingBG } from '../../Assets/svg/LandingBG'
 import { Section } from '../../Reusables/Section'
+import Thiruvalluvar from '../../Assets/Images/Thiruvalluvar.png'
+import './HeroBanner.css'
+import { Button } from '../../Reusables/Button'
 
 export const HeroBanner = () => {
-    return <Section>Hero Banner</Section>
+    return (
+        <div className="hero">
+            <div className="hero__bg">
+                <LandingBG />
+            </div>
+            <div className="hero__logo">Logo</div>
+            <div className="container">
+                <div className="hero__text">
+                    <h1 className="hero__primary">
+                        Synsara 20<sup>20</sup>
+                    </h1>
+                    <p className="hero__secondary">
+                        Curabitur non nulla sit amet nisl tempus convallis quis
+                        ac lectus. Praesent sapien massa.
+                    </p>
+                    <Button>View Events</Button>
+                </div>
+                <div>
+                    <img
+                        src={Thiruvalluvar}
+                        className="img-liquid hero__img"
+                        alt=""
+                    />
+                </div>
+            </div>
+        </div>
+    )
 }
