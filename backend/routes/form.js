c3b04341d7c92aefe5d7d418bd30c843b188ec7a
VasanthKumar14 <vasanthkumar14.99@gmail.com>
2020-09-08 14:49:03 +0530

    Node mailer component added

--- backend/routes/form.js
+++ backend/routes/form.js
@@ -1,43 +1,30 @@
 const express = require('express')
 
 const Participants = require('../models/participants')
+const mailer = require('../components/mail')
 
 const formRouter = express.Router()
 
-formRouter
-    .route('/')
-    .get((req, res, next) => {
-        console.log('get')
-        Participants.find()
-            .then(
-                (participants) => {
-                    res.statusCode = 200
-                    res.setHeader('Content-Type', 'application/json')
-                    res.json(participants)
-                },
-                (err) => next(err)
-            )
-            .catch((err) => next(err))
-    })
-    .post((req, res, next) => {
-        console.log('post')
-        Participants.create(req.body)
-            .then(
-                (participant) => {
-                    console.log('Participant Registered', participant)
-                    res.statusCode = 200
-                    res.setHeader('Content-Type', 'application/json')
-                    res.json(participant)
-                },
-                (err) => {
-                    console.log('error')
-                    next(err)
-                }
-            )
-            .catch((err) => {
+formRouter.route('/').post((req, res, next) => {
+    console.log('post')
+    Participants.create(req.body)
+        .then(
+            (participant) => {
+                console.log('Participant Registered', participant)
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json(participant)
+                mailer.sendmail('vasanthkumar14.99@gmail.com', 'hello') //participant email and event name must be added
+            },
+            (err) => {
                 console.log('error')
                 next(err)
-            })
-    })
+            }
+        )
+        .catch((err) => {
+            console.log('error')
+            next(err)
+        })
+})
 
 module.exports = formRouter
