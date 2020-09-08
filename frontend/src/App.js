a3f20bc76907b502dc20bc83151df539d66ce241
dineshsy <s.ydinesh1177@gmail.com>
2020-09-08 21:47:03 +0530

    Removed Warnings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { index as LandingPage } from './LandingPage'
 import { index as ComingSoon } from './ComingSoon'
-import { ThemeProvider } from 'styled-components'
-import { theme } from './utils/globalStyles'
 
 function App() {
     return <LandingPage /> && <ComingSoon />
