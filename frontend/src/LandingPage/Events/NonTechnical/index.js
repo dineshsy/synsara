f390839ba004bb919a26a64061aeaa24b99f1533
Vijai <vijayselvanvj@gmail.com>
2020-09-15 16:38:24 +0530

    Updated contents of non tech events

--- frontend/src/LandingPage/Events/NonTechnical/index.js
+++ frontend/src/LandingPage/Events/NonTechnical/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Gaming } from './Gaming/Gaming'
+import { Photography } from './Photography/Photography'
+import { Treasurehunt } from './Treasurehunt/Treasurehunt'
+import { Connections } from './Connections/Connections'
+import { Instareels } from './Instareels/Instareels'
+import { Memecontest } from './Memecontest/Memecontest'
+import { Dream11IPL } from './Dream11IPL/Dream11IPL'
 import styled from 'styled-components'
 import { sizeMaxH, sizeMaxW } from '../../../utils/MediaQueires'
 const SliderWrapper = styled.section`
     width: 100%;
     height: 100%;
-    padding: 5rem;
     @media screen and ${sizeMaxH.tablet} and ${sizeMaxW.tablet} {
         padding: 2rem;
     }
@@ -21,7 +26,32 @@ export const Index = () => {
             </div>
             <div className="slide">
                 <SliderWrapper>
-                    <Gaming />
+                    <Photography />
+                </SliderWrapper>
+            </div>
+            <div className="slide">
+                <SliderWrapper>
+                    <Treasurehunt />
+                </SliderWrapper>
+            </div>
+            <div className="slide">
+                <SliderWrapper>
+                    <Connections />
+                </SliderWrapper>
+            </div>
+            <div className="slide">
+                <SliderWrapper>
+                    <Instareels />
+                </SliderWrapper>
+            </div>
+            <div className="slide">
+                <SliderWrapper>
+                    <Memecontest />
+                </SliderWrapper>
+            </div>
+            <div className="slide">
+                <SliderWrapper>
+                    <Dream11IPL />
                 </SliderWrapper>
             </div>
         </>
